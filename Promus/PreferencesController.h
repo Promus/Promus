6e370c788f9fb7b13893e5b22f28c51e5a7ef197
Jeremiah Smith <jfrotscher@gmail.com>
2013-12-11 17:42:52 +0100

    10 Dec 2013 – Fixed about menu, menu items in menulet, cleared views, corrected connections

--- Promus/PreferencesController.h
+++ Promus/PreferencesController.h
@@ -7,26 +7,26 @@
 #import "MenuCalls.h"
 
 extern NSString* GeneralToolbarItemIdentifier;
-extern NSString* CleanToolbarItemIdentifier;
-extern NSString* BatchToolbarItemIdentifier;
-extern NSString* TimerItemIdentifier;
-extern NSString* SecurityToolbarItemIdentifier;
-extern NSString* ReportsToolbarItemIdentifier;
-extern NSString* ConsoleToolbarItemIdentifier;
-extern NSString* SoftwareUpdatesToolbarItemIdentifier;
+extern NSString *CleanToolbarItemIdentifier;
+extern NSString *BatchToolbarItemIdentifier;
+extern NSString *TimerItemIdentifier;
+extern NSString *SecurityToolbarItemIdentifier;
+extern NSString *ReportsToolbarItemIdentifier;
+extern NSString *ConsoleToolbarItemIdentifier;
+extern NSString *SoftwareUpdatesToolbarItemIdentifier;
 
 @interface PreferencesController : NSWindowController
 {
-  IBOutlet NSView* generalView;
-  IBOutlet NSView* cleanView;
-  IBOutlet NSView* batchView;
-  IBOutlet NSView* timerView;
-  IBOutlet NSView* securityView;
-  IBOutlet NSView* reportsView;
-  IBOutlet NSView* consoleView;
-  IBOutlet NSView* softwareupdatesView;
-  IBOutlet NSView* emptyView;
-  NSMutableDictionary* toolbarItems;
+  IBOutlet NSView *generalView;
+  IBOutlet NSView *cleanView;
+  IBOutlet NSView *batchView;
+  IBOutlet NSView *timerView;
+  IBOutlet NSView *securityView;
+  IBOutlet NSView *reportsView;
+  IBOutlet NSView *consoleView;
+  IBOutlet NSView *softwareupdatesView;
+  IBOutlet NSView *emptyView;
+  NSMutableDictionary *toolbarItems;
 }
 
 -(IBAction) toolbarHit:(id)sender;
